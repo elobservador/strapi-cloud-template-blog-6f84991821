d383ed0acc0f1814aed4e59eacfe91f26827a2b7
Gaston De la llana <gastondelallana@MacBook-Pro-de-Gaston.local>
2025-04-19 23:05:54 -0300

    preview plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,65 @@
-module.exports = () => ({});
+module.exports = () => ({
+    'preview-button': {
+        config: {
+            contentTypes: [
+                {
+                    uid: 'api::eo-acordeon.eo-acordeon',
+                    draft: {
+                        url: 'https://especiales.elobservador.com.uy/interactivo/widgets/preview/',
+                        query: {
+                            type: 'eo-acordeons',
+                            id: '{documentId}',
+                        },
+                        copy: false,
+                    },
+                    published: {
+                        url: '',
+                        query: {
+                            type: 'eo-acordeons',
+                            id: '{documentId}',
+                        },
+                        copy: false,
+                    },
+                },
+                {
+                    uid: 'api::eo-columnista.eo-columnista',
+                    draft: {
+                        url: 'https://especiales.elobservador.com.uy/interactivo/widgets/preview/',
+                        query: {
+                            type: 'eo-columnistas',
+                            id: '{documentId}',
+                        },
+                        copy: false,
+                    },
+                    published: {
+                        url: '',
+                        query: {
+                            type: 'eo-columnistas',
+                            id: '{documentId}',
+                        },
+                        copy: false,
+                    },
+                },
+                {
+                    uid: 'api::eo-multimedia-carrusel.eo-multimedia-carrusel',
+                    draft: {
+                        url: 'https://especiales.elobservador.com.uy/interactivo/widgets/preview/',
+                        query: {
+                            type: 'eo-multimedia-carrusels',
+                            id: '{documentId}',
+                        },
+                        copy: false,
+                    },
+                    published: {
+                        url: '',
+                        query: {
+                            type: 'eo-multimedia-carrusels',
+                            id: '{documentId}',
+                        },
+                        copy: false,
+                    },
+                },
+            ]
+        }
+    }
+});
